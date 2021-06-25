23a88db01575f992be182cc2401134c98afe45af
mmanso <mayara.manso@mercadolivre.com>
2021-06-25 01:52:34 -0300

    config Babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "presets": [
-    "@babel/env",
-    "@babel/react"
+    "@babel/preset-env",
+    "@babel/preset-react"
   ],
   "plugins": [
     "@babel/plugin-proposal-class-properties"
