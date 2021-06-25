c1b1d8ac4b9b176d53d39cce09f77253ed1f700f
mmanso <mayara.manso@mercadolivre.com>
2021-06-25 02:03:25 -0300

    render app with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import './assets/fonts/FuturaNow/FuturaNowHeadline-Bd.ttf'
+
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+  , document.getElementById('root'));
