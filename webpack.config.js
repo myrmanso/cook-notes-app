567d689138042ab82e4d8552385e7626a908f731
mmanso <mayara.manso@mercadolivre.com>
2021-06-25 01:53:08 -0300

    load jsx, jpg, png, ttf and react-router

--- webpack.config.js
+++ webpack.config.js
@@ -9,25 +9,52 @@ module.exports = {
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
     inline: true,
     port: 3000
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
+          }
+        }
       },
       {
-        test: /\.(css|scss)$/,
-        use: ['style-loader", "css-loader'],
+        test: /\.(jpg|png)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'file-loader',
+        }
       },
       {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: ['file-loader']
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
       },
+      {
+        test: /\.(woff(2)?|ttf|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: '[name].[ext]',
+          outputPath: 'assets'
+        }
+      }
     ],
+
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
