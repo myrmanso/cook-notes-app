591d92192b35f7a4c699cbd5df5e7eb0068d1c0e
mmanso <mayara.manso@mercadolivre.com>
2021-07-02 04:21:04 -0300

    creates SignupPages

--- src/pages/SignupPages.js
+++ src/pages/SignupPages.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import AuthenticationTemplade from '../components/templades/AuthenticationTemplade';
 
-const SignupPages = () => {
-  const handleLoginUser = (values) => console.log(values)
+import AuthenticationTemplade from '../components/templades/AuthenticationTemplade';
+import apiService from '../services/api.services';
+
+const SignupPages = (props) => {
+  const handleCreateUser = async values => {
+    try {
+      const { result, token } = await apiService.signupUser(values);
+
+      localStorage.setItem('token', token);
+      localStorage.setItem('result', result);
+
+      props.history.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <AuthenticationTemplade
-      handleLoginUser={handleLoginUser}
+      handleLoginUser={handleCreateUser}
     />
   );
 };
