0512a5565bbff2fb12cdcfb94506d3a120a7c2e3
mmanso <mayara.manso@mercadolivre.com>
2021-07-02 04:17:45 -0300

    update CardRecipe style

--- src/components/organismos/CardRecipe.js
+++ src/components/organismos/CardRecipe.js
@@ -5,20 +5,21 @@ import Button from '../moleculas/Button'
 import ImageCard from '../moleculas/ImageCard'
 import Player from '../moleculas/Player'
 
-const CardRecipe = ({ url, title, addRecipe }) => (
+const CardRecipe = ({ url, title, addRecipe, editRecipeUrl }) => (
   <section className="card-recipes">
-    <div>
+    <div className="card-recipes__content">
       {isYoutube(url) &&
         <Player url={url} />
       }
       <ImageCard url={url} />
       {url
-        ? <Ancor className="card-recipes__title" href={url, "_blank"}>{title}</Ancor>
-        : <h4 className="card-recipes__title">{title}</h4>
+        ? <Ancor className="card-recipes__content__title" href={url} target={"_blank"}>{title}</Ancor>
+        : <h4 className="card-recipes__content__title">{title}</h4>
       }
     </div>
     <div className="card-recipes__button">
-      <Button action={addRecipe}>Add</Button>
+      {addRecipe && <Button action={addRecipe}>Adicionar</Button>}
+      {editRecipeUrl && <Ancor href={editRecipeUrl} className="card-recipes__button__ancor">Editar</Ancor>}
     </div>
   </section>
 );
