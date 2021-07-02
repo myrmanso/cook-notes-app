3f4081567ff743b1ab0baad0d7a4cbb8a084bd87
mmanso <mayara.manso@mercadolivre.com>
2021-07-02 04:15:05 -0300

    creates 2 new route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import './index.scss'
-import CreateRecipe from './pages/CreateRecipe';
-import LoginPages from './pages/LoginPages';
+import CreateRecipe from './pages/CreateRecipePage';
+import LoginPages from './pages/LoginPage';
 import RecipesPage from './pages/RecipesPage';
 import SignupPages from './pages/SignupPages';
+import UserRecipesPage from './pages/UserRecipesPage';
+import UpdateRecipePage from './pages/UpdateRecipePage';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={RecipesPage} />
+    <Route exact path="/user" component={UserRecipesPage} />
     <Route exact path="/recipe" component={CreateRecipe} />
+    <Route exact path="/recipe/:recipeId" component={UpdateRecipePage} />
     <Route exact path="/signup" component={SignupPages} />
     <Route exact path="/login" component={LoginPages} />
   </Switch>
