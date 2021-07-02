91ea67c204c31ce1ef5b05191ffa63fd488f4659
mmanso <mayara.manso@mercadolivre.com>
2021-07-02 04:20:16 -0300

    creates LoginPage

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import AuthenticationTemplade from '../components/templades/AuthenticationTemplade';
+import apiService from '../services/api.services';
+
+const LoginPage = (props) => {
+  const handleLoginUser = async values => {
+    const { email, password } = values;
+    console.log(values);
+    try {
+      const { result, message } = await apiService.loginUser({ email, password });
+
+      localStorage.setItem('token', message);
+      localStorage.setItem('result', result);
+
+      props.history.push('/');
+    } catch (error) {
+      if (error.response.data.message === 'Invalid credentials') {
+        console.log(error.response.data.message)
+      }
+    }
+  }
+  return (
+    <AuthenticationTemplade
+      isLogin
+      handleLoginUser={handleLoginUser}
+    />
+  );
+};
+
+export default LoginPage;
