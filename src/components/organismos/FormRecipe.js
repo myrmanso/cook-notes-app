9f36a9be0797145212fee30a6066c57707b504bd
mmanso <mayara.manso@mercadolivre.com>
2021-07-02 04:18:20 -0300

    delets unsual filters

--- src/components/organismos/FormRecipe.js
+++ src/components/organismos/FormRecipe.js
@@ -4,11 +4,6 @@ import { useFormik } from 'formik';
 
 import Input from '../moleculas/Input';
 import Button from '../moleculas/Button';
-import Ancor from '../moleculas/Ancor';
-
-
-import { availableFilters } from '../../mockAvailableFilters';
-import { filterById } from '../../helpers/filterById';
 
 const formSchema = Yup.object().shape({
   name: Yup.string()
@@ -16,33 +11,25 @@ const formSchema = Yup.object().shape({
     .min(3, 'Mínimo de 3 caracteres')
     .max(100, 'Máximo de 100 caracteres')
     .required('Campo obrigatório'),
-  description: Yup.string()
-    .trim()
-    .max(Infinity),
   externalLink: Yup.string()
     .trim()
-    .url('Digite uma url válida'),
+    .url('Digite uma url válida')
+    .required('Campo obrigatório'),
   imgUrl: Yup.string()
     .trim()
-    .url('Digite uma url válida'),
+    .url('Digite uma url válida')
+    .required('Campo obrigatório'),
 });
 
-const FormRecipes = ({ handleRecipe }) => {
-  const filterLevel = filterById(availableFilters, 'level');
-  const filterMeals = filterById(availableFilters, 'meals');
-  const filterFlavor = filterById(availableFilters, 'flavor');
-  const filterOccasion = filterById(availableFilters, 'occasion');
-
+const FormRecipes = (props) => {
+  const { handleRecipe, buttonLabel, name, externalLink, imgUrl } = props;
   const formik = useFormik({
     initialValues: {
       name: '',
       externalLink: '',
       imgUrl: '',
-      level: '',
-      occasion: '',
-      meals: '',
-      flavor: '',
     },
+
     onSubmit: (values) => {
       handleRecipe(values);
     },
@@ -62,7 +49,7 @@ const FormRecipes = ({ handleRecipe }) => {
           type="text"
           onChange={formik.handleChange}
           onBLur={formik.handleBlur}
-          value={formik.values.name}
+          value={formik.values.name || name}
           error={formik.errors.name}
         />
       </div>
@@ -77,7 +64,7 @@ const FormRecipes = ({ handleRecipe }) => {
           type="url"
           onBLur={formik.handleBlur}
           onChange={formik.handleChange}
-          value={formik.values.externalLink}
+          value={formik.values.externalLink || externalLink}
           error={formik.errors.externalLink}
         />
       </div>
@@ -90,102 +77,19 @@ const FormRecipes = ({ handleRecipe }) => {
           className={`form-recipe__input ${formik.errors.imgUrl && 'error'}`}
           placeholder=""
           type="url"
-          value={formik.values.imgUrl}
+          value={formik.values.imgUrl || imgUrl}
           onChange={formik.handleChange}
           onBLur={formik.handleBlur}
           error={formik.errors.imgUrl}
         />
       </div>
-      <div className="form-recipe__container--flex">
-        <div className="form-recipe__container--checkbox">
-          <p>Nível:</p>
-          {filterLevel[0].values.map(value => (
-            <label className="form-recipe__label">
-              <Input
-                id={value}
-                key={value}
-                name="flavor"
-                className="form-recipe__input"
-                placeholder=""
-                type="checkbox"
-                value={value}
-                checked={formik.values.level}
-                onChange={formik.handleChange}
-                onBLur={formik.handleBlur}
-              />
-              {value}
-            </label>))}
-        </div>
-        <div className="form-recipe__container--checkbox">
-          <p>Refeição:</p>
-          {filterMeals[0].values.map(value => (
-            <label className="form-recipe__label">
-              <Input
-                id={value}
-                key={value}
-                name="meals"
-                className="form-recipe__input"
-                placeholder=""
-                type="checkbox"
-                value={value}
-                checked={formik.values.meals}
-                onChange={formik.handleChange}
-                onBLur={formik.handleBlur}
-              />
-              {value}
-            </label>
-          ))}
-        </div>
-        <div className="form-recipe__container--checkbox">
-          <p>Sabor:</p>
-          {filterFlavor[0].values.map(value => (
-            <label className="form-recipe__label">
-              <Input
-                id={value}
-                key={value}
-                name="flavor"
-                className="form-recipe__input"
-                placeholder=""
-                type="checkbox"
-                value={value}
-                checked={formik.values.flavor}
-                onChange={formik.handleChange}
-                onBLur={formik.handleBlur}
-              />
-              {value}
-            </label>))}
-        </div>
-        <div className="form-recipe__container--checkbox">
-          <p>Datas comemorativas: </p>
-          {filterOccasion[0].values.map(value => (
-            <label className="form-recipe__label">
-              <Input
-                id={value}
-                key={value}
-                name="occasion"
-                className="form-recipe__input"
-                placeholder=""
-                type="checkbox"
-                checked={formik.values.occasion}
-                value={value}
-                onChange={formik.handleChange}
-                onBLur={formik.handleBlur}
-              />
-              {value}
-            </label>))}
-        </div>
-      </div>
       <div className="form-recipe__container--button">
-        <Ancor
-          href="/"
+        <Button
+          className="form-recipe__button"
+          type="submit"
         >
-          <Button
-            className="form-recipe__button"
-            type="submit"
-          >
-            Criar
+          {buttonLabel}
         </Button>
-        </Ancor>
       </div>
     </form >
   );
