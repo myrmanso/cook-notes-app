9d0541c5da827b7a2452561020e66135a9b90fd6
mmanso <mayara.manso@mercadolivre.com>
2021-07-02 04:18:53 -0300

    create logout

--- src/components/templades/Navbar.js
+++ src/components/templades/Navbar.js
@@ -5,43 +5,51 @@ import SearchRecipes from '../organismos/SearchRecipes';
 import LogoIcon from '../../assets/icons/logoIcon';
 import UserIcon from '../../assets/icons/userIcon';
 import PlusIcon from '../../assets/icons/plusIcon';
+import Button from '../moleculas/Button';
 
-const Navbar = ({ handleSearch, showSearch }) => (
-  <header className="navbar">
-    <nav className="navbar__navigation">
-      <Ancor
-        href='/'
-        className="navbar__navigation__ancor-logo"
-      >
-        <LogoIcon width="50" height="50" />
+const Navbar = ({ handleSearch, showSearch, history }) => {
+  const logout = () => {
+    localStorage.clear();
+    history.push('/login');
+  }
+
+  return (
+    <header className="navbar">
+      <nav className="navbar__navigation">
+        <Ancor
+          href='/'
+          className="navbar__navigation__ancor-logo"
+        >
+          <LogoIcon width="50" height="50" />
+        </Ancor>
+        <Ancor
+          href='/'
+          className="navbar__navigation__link-to-page"
+        >
+          Início
       </Ancor>
-      <Ancor
-        href='/'
-        className="navbar__navigation__link-to-page"
-      >
-        Início
-      </Ancor>
-      {showSearch && <SearchRecipes handleSearch={handleSearch} />}
-      <Ancor
-        href='/recipe'
-        className="navbar__navigation__plus-icon"
-      >
-        <PlusIcon />
-      </Ancor>
-      <Ancor
-        href='/'
-        className="navbar__navigation__user-icon"
-      >
-        <UserIcon />
-      </Ancor>
-      <Ancor
-        href='/'
-        className="navbar__navigation__link-to-page"
-      >
-        Sair
-      </Ancor>
-    </nav>
-  </header>
-);
+        {showSearch && <SearchRecipes handleSearch={handleSearch} />}
+        <Ancor
+          href='/recipe'
+          className="navbar__navigation__plus-icon"
+        >
+          <PlusIcon />
+        </Ancor>
+        <Ancor
+          href={`/user`}
+          className="navbar__navigation__user-icon"
+        >
+          <UserIcon />
+        </Ancor>
+        <Button
+          action={() => logout()}
+          className="navbar__navigation__link-to-page"
+        >
+          Sair
+      </Button>
+      </nav>
+    </header>
+  )
+};
 
 export default Navbar;
